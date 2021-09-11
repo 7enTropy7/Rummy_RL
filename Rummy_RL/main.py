eb6d74958397a4cb7d5f3f740c7a31098ebf2a14
Unnikrishnan Menon <menon.uk1998@gmail.com>
2021-09-11 11:48:16 +0530

    Add files via upload

---
+++ Rummy_RL/main.py
@@ -0,0 +1,99 @@
+import pydealer
+from test import Agent
+import numpy as np
+from ppo_torch import Agent
+
+inverse_values = {
+    13:"King",
+    12:"Queen",
+    11:"Jack",
+    10:"10",
+    9:"9",
+    8:"8",
+    7:"7",
+    6:"6",
+    5:"5",
+    4:"4",
+    3:"3",
+    2:"2",
+    1:"Ace"
+}
+
+inverse_suits = {
+    4:"Spades",
+    3:"Hearts",
+    2:"Clubs",
+    1:"Diamonds"
+}
+
+deck = pydealer.Deck()
+
+deck.shuffle()
+cards = deck.deal(10)
+
+hand = pydealer.Stack()
+hand.add(cards)
+
+agent = Agent(hand)
+
+print(agent.matrix)
+
+agent.get_binary_matrix()
+
+index = int(input('index: '))
+
+print(agent.card_from_index(index))
+
+# card = agent.drop_card_from_hand()
+# value, suit = np.argmax(card[0])
+# card_to_drop = pydealer.Card(inverse_values[value+1], inverse_suits[suit+1])
+# print(card_to_drop)
+
+
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v0')
+    N = 20
+    batch_size = 5
+    n_epochs = 4
+    alpha = 0.0003
+    agent = Agent(n_actions=env.action_space.n, batch_size=batch_size, 
+                    alpha=alpha, n_epochs=n_epochs, 
+                    input_dims=env.observation_space.shape)
+    n_games = 100
+
+    figure_file = 'plots/cartpole.png'
+
+    best_score = env.reward_range[0]
+    score_history = []
+
+    learn_iters = 0
+    avg_score = 0
+    n_steps = 0
+
+    for i in range(n_games):
+        observation = env.reset()
+        done = False
+        score = 0
+        while not done:
+            action, prob, val = agent.choose_action(observation)
+            print('action:', action, 'prob:', prob, 'val:', val)
+            observation_, reward, done, info = env.step(action)
+            n_steps += 1
+            score += reward
+            agent.remember(observation, action, prob, val, reward, done)
+            if n_steps % N == 0:
+                agent.learn()
+                learn_iters += 1
+            observation = observation_
+        score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
+
+        if avg_score > best_score:
+            best_score = avg_score
+            agent.save_models()
+
+        print('episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score,
+                'time_steps', n_steps, 'learning_steps', learn_iters)
+    x = [i+1 for i in range(len(score_history))]
+    plot_learning_curve(x, score_history, figure_file)
