77b58c38ad788e4271f3a192b79731147512d826
Unnikrishnan Menon <menon.uk1998@gmail.com>
2021-11-07 11:38:24 +0530

    reset script added

--- Federated Rummy/Client_C/reset.py
+++
@@ -1,5 +0,0 @@
-import os
-
-dir = 'local_ckpts'
-for f in os.listdir(dir):
-    os.remove(os.path.join(dir, f))
---
+++ Federated Rummy/reset.py
@@ -0,0 +1,41 @@
+import os
+
+if not os.path.exists('Server/global_ckpt'):
+    os.makedirs('Server/global_ckpt')
+
+if not os.path.exists('Server/incoming_ckpts'):
+    os.makedirs('Server/incoming_ckpts')
+
+if not os.path.exists('Client_A/local_ckpts'):
+    os.makedirs('Client_A/local_ckpts')
+
+if not os.path.exists('Client_B/local_ckpts'):
+    os.makedirs('Client_B/local_ckpts')
+
+if not os.path.exists('Client_C/local_ckpts'):
+    os.makedirs('Client_C/local_ckpts')
+
+
+dir = 'Server/incoming_ckpts/actors'
+for f in os.listdir(dir):
+    os.remove(os.path.join(dir, f))
+
+dir = 'Server/incoming_ckpts/critics'
+for f in os.listdir(dir):
+    os.remove(os.path.join(dir, f))
+
+dir = 'Server/global_ckpt'
+for f in os.listdir(dir):
+    os.remove(os.path.join(dir, f))
+
+dir = 'Client_A/local_ckpts'
+for f in os.listdir(dir):
+    os.remove(os.path.join(dir, f))
+
+dir = 'Client_B/local_ckpts'
+for f in os.listdir(dir):
+    os.remove(os.path.join(dir, f))
+
+dir = 'Client_C/local_ckpts'
+for f in os.listdir(dir):
+    os.remove(os.path.join(dir, f))
